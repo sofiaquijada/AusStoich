a38c149c389566c24eb353f4356ee59bb12cdaa9
Sofia Quijada <fsofiaq2003@gmail.com>
2024-05-24 16:22:33 -0400

    New script creation.

---
+++ Phylogeny visualization.R
@@ -0,0 +1,11 @@
+install.packages("ape")
+install.packages("phytools")
+install.packages("nlme")
+library(ape)
+library(phytools)
+library(nlme)
+library(visreg)
+library(dplyr)
+
+ITS_tree <- read.nexus("ITS_tree.tre")
+
